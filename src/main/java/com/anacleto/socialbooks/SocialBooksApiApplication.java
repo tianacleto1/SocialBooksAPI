70749308ba060f53447cf7331f194ab4ee4ba84d
Tiago <tianacleto@hotmail.com>
2018-06-27 16:13:52 -0300

    Initial commit

---
+++ src/main/java/com/anacleto/socialbooks/SocialBooksApiApplication.java
@@ -0,0 +1,12 @@
+package com.anacleto.socialbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBooksApiApplication.class, args);
+	}
+}
