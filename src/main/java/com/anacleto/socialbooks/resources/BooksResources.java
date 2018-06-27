b5527081e1a91d0ab0e0d88804a0f8593a9394cf
Tiago <tianacleto@hotmail.com>
2018-06-27 16:41:39 -0300

    Edited README.md

---
+++ src/main/java/com/anacleto/socialbooks/resources/BooksResources.java
@@ -0,0 +1,15 @@
+package com.anacleto.socialbooks.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksResources {
+	
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String list() {
+		return "Rest Applied, GIT Step By Step";
+	}
+
+}
