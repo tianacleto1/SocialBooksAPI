ec793a697b3133e6f30b4c5f8e77e424a3695eb6
Tiago <tianacleto@hotmail.com>
2018-06-27 17:50:06 -0300

    Added model objects classes

---
+++ src/main/java/com/anacleto/socialbooks/model/Book.java
@@ -0,0 +1,71 @@
+package com.anacleto.socialbooks.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Book {
+
+	private Long id;
+	private String name;
+	private Date publicationDate;
+	private String publishing;
+	private String summary;
+	private List<Review> reviews;
+	private String author;
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+	
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	
+	public String getPublishing() {
+		return publishing;
+	}
+	
+	public void setPublishing(String publishing) {
+		this.publishing = publishing;
+	}
+	
+	public String getSummary() {
+		return summary;
+	}
+	
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	public List<Review> getReviews() {
+		return reviews;
+	}
+	
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
--- src/main/java/com/anacleto/socialbooks/resources/BooksResources.java
+++ src/main/java/com/anacleto/socialbooks/resources/BooksResources.java
@@ -1,14 +1,29 @@
 package com.anacleto.socialbooks.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.anacleto.socialbooks.model.Book;
 
 @RestController
 public class BooksResources {
 	
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
-	public String list() {
-		return "Rest Applied, GIT Step By Step";
+	public List<Book> list() {
+		Book book1 = new Book();
+		book1.setId(1L);
+		book1.setName("Applied Rest");
+		
+		Book book2 = new Book();
+		book2.setId(2L);
+		book2.setName("GIT Step By Step");
+		
+		List<Book> books = new ArrayList<>();
+		books.add(book1);
+		books.add(book2);
+		
+		return books;
 	}
 }
