acf9f0444a06c544a7e120eb2e35a673326565f5
Tiago <tianacleto@hotmail.com>
2018-07-04 21:50:07 -0300

    Added ResponseEntity support

--- src/main/java/com/anacleto/socialbooks/repository/BookRepository.java
+++ src/main/java/com/anacleto/socialbooks/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.anacleto.socialbooks.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.anacleto.socialbooks.model.Book;
 
-public interface BooksRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
+	Book getById(Long id);
 }
--- src/main/java/com/anacleto/socialbooks/resources/BookResource.java
+++ src/main/java/com/anacleto/socialbooks/resources/BookResource.java
@@ -1,46 +1,62 @@
 package com.anacleto.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.anacleto.socialbooks.model.Book;
-import com.anacleto.socialbooks.repository.BooksRepository;
+import com.anacleto.socialbooks.repository.BookRepository;
 
 @RestController
 @RequestMapping("/books")
-public class BooksResources {
+public class BookResource {
 	
 	@Autowired
-	private BooksRepository booksRepository;
+	private BookRepository bookRepository;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Book> list() {
-		return booksRepository.findAll();
+		return bookRepository.findAll();
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void insert(@RequestBody Book book) {
-		booksRepository.save(book);
+	public ResponseEntity<Void> insert(@RequestBody Book book) {
+		book = bookRepository.save(book);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+											 .path("/{id}")
+											 .buildAndExpand(book.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Optional<Book> getBookById(@PathVariable("id") Long id) {
-		return booksRepository.findById(id);
+	public ResponseEntity<?> getBookById(@PathVariable("id") Long id) {
+		Book book = bookRepository.getById(id);
+		
+		if (book == null) 
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.status(HttpStatus.OK).body(book);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void deleteBook(@PathVariable("id") Long id) {
-		booksRepository.deleteById(id);
+		bookRepository.deleteById(id);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) 
 	public void updateBook(@RequestBody Book book, @PathVariable("id") Long id) {
 		book.setId(id);
-		booksRepository.save(book);
+		bookRepository.save(book);
 	}
 }
